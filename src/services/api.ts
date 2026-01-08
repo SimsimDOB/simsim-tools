a24fa39f5f65497068dbcda9e2f126a399823a56
FernandoYu <fyu@fi.uba.ar>
2026-01-08 04:28:29 -0300

    feat: update environment variables and add Pinger component for API pinging functionality

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:10827/api";
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:10827/api";
 
 const api = axios.create({
   baseURL: baseURL,
